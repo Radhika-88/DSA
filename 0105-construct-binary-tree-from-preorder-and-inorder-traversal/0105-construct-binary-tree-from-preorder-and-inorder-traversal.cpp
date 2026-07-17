7ffa5eb3cab62ceb48c00389d96ecc931b896a08
Radhika-88 <radhikamangla422@gmail.com>
2026-07-17 11:21:17 +0530

    Time: 3 ms (73.11%), Space: 27.5 MB (54.5%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -15,43 +15,29 @@ public:
     TreeNode* solve(vector<int>& preorder, vector<int>& inorder,
                     int &preIndex, int inStart, int inEnd,
                     unordered_map<int, int> &mp) {
-
         // Base case
         if (inStart > inEnd) {
             return NULL;
         }
-
         // Create the root node
         TreeNode* root = new TreeNode(preorder[preIndex]);
-
         // Find the position of root in inorder
         int pos = mp[preorder[preIndex]];
-
         // Move to the next element in preorder
         preIndex++;
-
         // Build left subtree
-        root->left = solve(preorder, inorder, preIndex,
-                           inStart, pos - 1, mp);
-
+        root->left = solve(preorder, inorder, preIndex,inStart, pos - 1, mp);
         // Build right subtree
-        root->right = solve(preorder, inorder, preIndex,
-                            pos + 1, inEnd, mp);
-
+        root->right = solve(preorder, inorder, preIndex,pos + 1, inEnd, mp);
         return root;
     }
-
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-
         unordered_map<int, int> mp;
-
         // Store inorder value -> index
         for (int i = 0; i < inorder.size(); i++) {
             mp[inorder[i]] = i;
         }
-
         int preIndex = 0;
-
         return solve(preorder, inorder, preIndex,
                      0, inorder.size() - 1, mp);
     }
