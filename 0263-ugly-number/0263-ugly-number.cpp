21c5e46115dc4e3d49b8a40172c97e3ca356d721
Radhika-88 <radhikamangla422@gmail.com>
2026-07-18 12:48:22 +0530

    Time: 2 ms (6.47%), Space: 7.8 MB (44.54%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int multipleOf(int n){
+         while(n%2 == 0){
+            n /= 2;
+        }
+        while(n%3 == 0){
+            n /= 3;
+        }
+        while(n%5 == 0){
+            n /= 5;
+        }
+        return n;
+    }
+    bool isUgly(int n) {
+        if(n <= 0)
+            return false;
+
+        return multipleOf(n) == 1;
+    }
+};
