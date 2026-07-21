4cc90239caa0d04e6bc15fdd2aceadda3ff58d50
Radhika-88 <radhikamangla422@gmail.com>
2026-07-21 11:13:38 +0530

    Time: 0 ms (100%), Space: 8.9 MB (37.71%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int num = n;
-        int ans;
-     for(int i = 1;i<=n;i++){
-        if(num >= i){
-            num = num -i;
-        }
-        else{
-            ans = i-1;
-            break;
-            }
-        } 
-        return  ans;
+        return (sqrt(8.0 * n + 1) - 1) / 2;   // AP series and inequality
     }
 };
